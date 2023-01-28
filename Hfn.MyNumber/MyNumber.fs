15b6124487b1bd076c4f42474d7a2bbc05a050f3
hironorinakazato <hironorinakazato@icloud.com>
2023-01-28 14:27:00 +0900

    Refactor

--- Hfn.MyNumber/MyNumber.fs
+++ Hfn.MyNumber/MyNumber.fs
@@ -5,18 +5,27 @@
 /// </summary>
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module MyNumber =
+    let private length = 12
+    let private notIncludeCheckDigitLength = 11
+
+    let private (|IsLengthCheckInvalid|_|) length input =
+        if input |> Seq.length <> length then Some(input) else None
+    let private (|IsNumberCheckInvalid|_|) input =
+        if input |> Seq.forall (fun x -> System.Char.IsNumber x) = false then Some(input) else None
+
     /// <summary>Calculate the check digit.</summary>
     [<CompiledName("CalculateCheckDigit")>]
     let calculateCheckDigit (myNumberNotIncludeCheckDigit: string) =
-        if myNumberNotIncludeCheckDigit = null then None
-        elif myNumberNotIncludeCheckDigit |> Seq.length <> 11 then None
-        elif myNumberNotIncludeCheckDigit |> Seq.forall (fun x -> System.Char.IsNumber x) = false then None
-        else
+        match myNumberNotIncludeCheckDigit with
+        | null -> None
+        | IsLengthCheckInvalid notIncludeCheckDigitLength _ -> None
+        | IsNumberCheckInvalid _ -> None
+        | _ ->
             let source =
                 myNumberNotIncludeCheckDigit
                 |> int64
                 |> Seq.unfold (fun x -> Some((%) x 10L, (/) x 10L))
-                |> Seq.take 11
+                |> Seq.take notIncludeCheckDigitLength
             let weights = [2L; 3L; 4L; 5L; 6L; 7L; 2L; 3L; 4L; 5L; 6L;]
             let result = (%) (source |> Seq.map2 (*) weights |> Seq.sum) 11L |> (-) 11L
             if result < 10L then result else 0L
@@ -26,12 +35,13 @@ module MyNumber =
     ///<summary>Verify the check digit valid.</summary>
     [<CompiledName("IsCheckDigitValid")>]
     let isCheckDigitValid (myNumber: string) =
-        if myNumber = null then false
-        elif myNumber |> Seq.length <> 12 then false
-        elif myNumber |> Seq.forall (fun x -> System.Char.IsNumber x) = false then false
-        else
-            let checkDigit = myNumber.Substring(0, 11) |> calculateCheckDigit
+        match myNumber with
+        | null -> false
+        | IsLengthCheckInvalid length _ -> false
+        | IsNumberCheckInvalid _ -> false
+        | _ ->
+            let checkDigit = myNumber.Substring(0, notIncludeCheckDigitLength) |> calculateCheckDigit
             if checkDigit.IsNone then false
             else
-                myNumber.Substring(11) |> int |> (=) checkDigit.Value
+                myNumber.Substring(notIncludeCheckDigitLength) |> int |> (=) checkDigit.Value
 
